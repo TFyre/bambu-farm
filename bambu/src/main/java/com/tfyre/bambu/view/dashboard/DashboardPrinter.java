5351c9874bbb97623c4864918c3de21a35a6a13b
Maxime Berduck <berduckm@3il.fr>
2024-06-17 08:40:33 +0200

    Refactor className
    Co-authored-by: Francois Steyn <fasteyn@gmail.com>

--- bambu/src/main/java/com/tfyre/bambu/view/dashboard/DashboardPrinter.java
+++ bambu/src/main/java/com/tfyre/bambu/view/dashboard/DashboardPrinter.java
@@ -317,22 +317,16 @@ public class DashboardPrinter implements NotificationHelper, ViewHelper {
             return;
         }
         printerStatus.setText(value);
+        printerStatus.removeClassName(LumoUtility.TextColor.ERROR);
+        printerStatus.removeClassName(LumoUtility.TextColor.PRIMARY);
+        printerStatus.removeClassName(LumoUtility.TextColor.SUCCESS);
         if (gcodeState.isError()) {
-            printerStatus.removeClassName(LumoUtility.TextColor.SUCCESS);
-            printerStatus.removeClassName(LumoUtility.TextColor.PRIMARY);
             printerStatus.addClassName(LumoUtility.TextColor.ERROR);
         } else if(gcodeState.isReady()){
-            printerStatus.removeClassName(LumoUtility.TextColor.ERROR);
-            printerStatus.removeClassName(LumoUtility.TextColor.PRIMARY);
             printerStatus.addClassName(LumoUtility.TextColor.SUCCESS);
         } else if(gcodeState.isPrinting()) {
-            printerStatus.removeClassName(LumoUtility.TextColor.ERROR);
-            printerStatus.removeClassName(LumoUtility.TextColor.SUCCESS);
             printerStatus.addClassName(LumoUtility.TextColor.PRIMARY);
-        } else {
-            printerStatus.removeClassName(LumoUtility.TextColor.ERROR);
-            printerStatus.removeClassName(LumoUtility.TextColor.SUCCESS);
-            printerStatus.removeClassName(LumoUtility.TextColor.PRIMARY);
+        }
         }
     }
 
